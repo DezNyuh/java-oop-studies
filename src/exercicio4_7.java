1454d8f5e0534b79bacd9fa96c971f76b3442f69
DezNyuh <devrh3424@gmail.com>
2026-03-29 23:35:27 -0300

    mais alguns exercicios simples

---
+++ src/exercicio4_7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercicio4_7 {
+	
+	public static void main (String[]args) {
+		
+		Scanner sc  = new Scanner (System.in);
+		
+int n = sc.nextInt();
+
+for(int i = n; i <=n; i++) {
+int primeiro = n;
+int segundo = n * n;
+int terceiro = n * n * n;
+System.out.printf("%d\n%d\n%d\n", primeiro, segundo, terceiro);
+}
+		
+		
+		
+		
+	}
+}
